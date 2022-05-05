e068ca5c49c68705ed449a93f05f6b6389582459
Joseph Cryer <jcryer@Josephs-MacBook-Pro.local>
2022-05-06 00:59:06 +0100

    Final commit

--- src/Graph/ConfirmModal.js
+++ src/Graph/ConfirmModal.js
@@ -1,11 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import { Typography } from '@mui/material';
 
 export default function ConfirmModal({ open, setOpen, reset }) {
